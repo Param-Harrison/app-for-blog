5302ac65158e881a761713db7c1b433da6537390
Param-Harrison <paramanantham@live.com>
2018-08-26 12:57:35 +0300

    feat: channel lists for styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled from 'emotion'
+import FeatherIcon from 'feather-icons-react'
 
 import Layout from '../components/layout'
 
+// Container components
 const Wrapper = styled.section`
   display: grid;
   grid-template-columns: repeat(12, 1fr);
@@ -12,7 +14,7 @@ const Wrapper = styled.section`
 const Sidebar = styled.aside`
   background: #dd2c00;
   color: #ffffff;
-  grid-column: span 3;
+  grid-column: span 2;
 `
 const BlogListContainer = styled.aside`
   background: #f1f6fb;
@@ -20,15 +22,55 @@ const BlogListContainer = styled.aside`
 `
 const BlogContainer = styled.aside`
   background: #fafafa;
-  grid-column: span 6;
+  grid-column: span 7;
+`
+
+const BaseContainer = styled.section`
+  padding: 0.5rem 1rem;
+`
+
+const Container = styled(BaseContainer)`
+  display: flex;
+`
+
+// Sidebar components
+const RightIcon = styled(FeatherIcon)`
+  margin-left: auto;
+`
+const ChannelList = BaseContainer.withComponent('ul')
+const Channel = styled.li`
+  display: flex;
+  cursor: pointer;
+  &:hover {
+    background: #fafafa;
+    color: #444;
+  }
+`
+const LeftIcon = styled(FeatherIcon)`
+  padding-right: 0.4rem;
 `
 
 const IndexPage = () => (
   <Layout>
     <Wrapper>
-      <Sidebar>Channels List</Sidebar>
-      <BlogListContainer>Blog List</BlogListContainer>
-      <BlogContainer>Blog Container</BlogContainer>
+      <Sidebar>
+        <Container>
+          <h4>Channels</h4>
+        </Container>
+        <ChannelList>
+          <Channel>
+            <LeftIcon icon="hash" size={18} />
+            <div>Channels</div>
+            <RightIcon icon="chevron-right" size={20} />
+          </Channel>
+        </ChannelList>
+      </Sidebar>
+      <BlogListContainer>
+        <Container>Blog List</Container>
+      </BlogListContainer>
+      <BlogContainer>
+        <Container>Blog Container</Container>
+      </BlogContainer>
     </Wrapper>
   </Layout>
 )
