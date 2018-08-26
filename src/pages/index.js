b7a0d08bffacd7df366e4c40955e307009fa7913
Param-Harrison <paramanantham@live.com>
2018-08-26 21:37:16 +0300

    feat: update the blog sections content

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,23 +12,21 @@ const Wrapper = styled.section`
   color: #444;
 `
 const Sidebar = styled.aside`
-  background: #dd2c00;
+  background: #654ea3;
   color: #ffffff;
   grid-column: span 2;
 `
 const BlogListContainer = styled.aside`
-  background: #f1f6fb;
+  background: #e9e4f0;
   grid-column: span 3;
 `
 const BlogContainer = styled.aside`
   background: #fafafa;
   grid-column: span 7;
 `
-
 const BaseContainer = styled.section`
   padding: 0.5rem 0.75rem;
 `
-
 const Container = styled(BaseContainer)`
   display: flex;
 `
@@ -41,18 +39,51 @@ const Channel = styled.li`
   display: flex;
   cursor: pointer;
   padding: 0.5rem 1rem;
-  transition: all 0.1s linear;
+  transition: all 0.2s linear;
   &:hover {
     background: rgba(255, 255, 255, 0.2);
   }
   &.active {
-    background: #f1f6fb;
-    color: #444;
+    background: linear-gradient(to right, #f12711, #f5af19);
+    color: #fff;
+    cursor: default;
   }
 `
 const LeftIcon = styled(FeatherIcon)`
   padding-right: 0.3rem;
   margin-right: 0.5rem;
+`
+
+// Blog list component
+const SingleBlogHead = styled.h4`
+  font-weight: normal;
+  color: #444;
+`
+const SingleBlogParagraph = styled.p`
+  color: #888;
+  padding-top: 0.2rem;
+  font-size: 0.8rem;
+`
+const SingleBlog = styled.div`
+  padding: 1rem;
+  margin: 0.6rem 0.8rem;
+  background: rgba(255, 255, 255, 0.4);
+  border-radius: 4px;
+  cursor: pointer;
+  transition: all 0.3s linear;
+  &:hover {
+    background: #fff;
+  }
+  &.active {
+    background: linear-gradient(to right, #f12711, #f5af19);
+    cursor: default;
+    ${SingleBlogHead} {
+      color: #fff;
+    }
+    ${SingleBlogParagraph} {
+      color: #ededed;
+    }
+  }
 `
 
 const IndexPage = () => (
@@ -87,7 +118,36 @@ const IndexPage = () => (
         </ChannelList>
       </Sidebar>
       <BlogListContainer>
-        <Container>Blog List</Container>
+        <SingleBlog>
+          <SingleBlogHead>About this website</SingleBlogHead>
+          <SingleBlogParagraph>Blog with web app UI</SingleBlogParagraph>
+        </SingleBlog>
+        <SingleBlog className="active">
+          <SingleBlogHead>
+            My daily routine in product development
+          </SingleBlogHead>
+          <SingleBlogParagraph>Simple words about product</SingleBlogParagraph>
+        </SingleBlog>
+        <SingleBlog>
+          <SingleBlogHead>About this website</SingleBlogHead>
+          <SingleBlogParagraph>Blog with web app UI</SingleBlogParagraph>
+        </SingleBlog>
+        <SingleBlog>
+          <SingleBlogHead>
+            My daily routine in product development
+          </SingleBlogHead>
+          <SingleBlogParagraph>Simple words about product</SingleBlogParagraph>
+        </SingleBlog>
+        <SingleBlog>
+          <SingleBlogHead>About this website</SingleBlogHead>
+          <SingleBlogParagraph>Blog with web app UI</SingleBlogParagraph>
+        </SingleBlog>
+        <SingleBlog>
+          <SingleBlogHead>
+            My daily routine in product development
+          </SingleBlogHead>
+          <SingleBlogParagraph>Simple words about product</SingleBlogParagraph>
+        </SingleBlog>
       </BlogListContainer>
       <BlogContainer>
         <Container>Blog Container</Container>
