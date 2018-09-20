62ede5b5826fec5a13829d92cb598056b7c9e971
Param-Harrison <paramanantham@live.com>
2018-09-20 20:56:41 +0530

    feat: topics added for initial blogs

--- src/data/topics.js
+++ src/data/topics.js
@@ -6,12 +6,51 @@ const topicData = {
       link: '/',
     },
     {
+      title: 'Work Experience & Education',
+      text: 'Life as a developer',
+      link: '/profile/work-history',
+    },
+    {
+      title: 'Business and Activities',
+      text: 'Interests other than programming',
+      link: '/profile/other-activities',
+    },
+    {
       title: 'About this website',
-      text: 'Blog with web app UI',
+      text: 'Blog with web app UI design?',
       link: '/profile/web-app-as-blog',
+    },
+    {
+      title: 'About my family',
+      link: '/profile/about-family',
     },
   ],
   development: [
+    {
+      title: 'Full stack proficiency',
+      text: 'Things to get the job done',
+      link: '/development/full-stack-development',
+    },
+    {
+      title: 'My (current) experiment full stack',
+      text: 'Horses for courses',
+      link: '/development/experimental-stack',
+    },
+    {
+      title: 'Experience at Jobbatical',
+      text: 'Learning more than programming',
+      link: '/development/jobbatical',
+    },
+    {
+      title: 'Experience at Ufaber',
+      text: 'Roles and responsibilities at ufaber.com',
+      link: '/development/ufaber',
+    },
+    {
+      title: 'Developed sports web and mobile products',
+      text: 'Learned the art of development',
+      link: '/development/sports-products',
+    },
     {
       title: 'List of all learning stuff',
       link: '/development/learning-list',
@@ -24,6 +63,9 @@ const topicData = {
       link: '/product/product-development',
     },
   ],
+  design: [],
+  goals: [],
+  philosophy: [],
 }
 
 export default topicData
